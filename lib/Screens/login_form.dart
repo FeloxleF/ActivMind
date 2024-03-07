887bf394ac1478626b224a5dd8cd556f21a757c2
alexnezami <alireza.nezami@gmail.com>
2024-03-07 17:02:28 -0500

    change home page

--- lib/Screens/login_form.dart
+++ lib/Screens/login_form.dart
@@ -3,11 +3,11 @@
 import 'package:activmind_app/Screens/HomeForm.dart';
 // import 'package:activmind_app/Screens/home.dart';
 import 'package:activmind_app/Screens/signup_form.dart';
+import 'package:activmind_app/Screens/Calendar.dart';
 import 'package:activmind_app/common/gen_text_form_field.dart';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'dart:convert';
-
 
 class LoginForm extends StatefulWidget {
   const LoginForm({super.key});
@@ -21,36 +21,37 @@ class _LoginFormState extends State<LoginForm> {
   final _conpassword = TextEditingController();
 
   Future<void> login() async {
-  final response = await http.post(
-    Uri.parse('http://127.0.0.1:8000/login/'),
-    headers: <String, String>{
-      'Content-Type': 'application/json; charset=UTF-8',
-    },
-    body: jsonEncode(<String, String>{
-      'email': _conemail.text,
-      'password': _conpassword.text,
-    }),
-  );
+    final response = await http.post(
+      Uri.parse('http://127.0.0.1:8000/login/'),
+      headers: <String, String>{
+        'Content-Type': 'application/json; charset=UTF-8',
+      },
+      body: jsonEncode(<String, String>{
+        'email': _conemail.text,
+        'password': _conpassword.text,
+      }),
+    );
 
-  final BuildContext currentContext = context;
-  if (response.statusCode == 200) {
-    // ignore: use_build_context_synchronously
-    Navigator.pushReplacement(currentContext, MaterialPageRoute(builder: (_) => const HomeForm()));
-  } else {
-    // print('Failed to login');
-    ScaffoldMessenger.of(currentContext).showSnackBar(
-    SnackBar(
-      content: Text('Please enter a valid username.'),
-      backgroundColor: Colors.red,
-    ),
-  );
-  }
+    final BuildContext currentContext = context;
+    if (response.statusCode == 200) {
+      // ignore: use_build_context_synchronously
+      Navigator.pushReplacement(
+          currentContext, MaterialPageRoute(builder: (_) => const HomeForm()));
+    } else {
+      // print('Failed to login');
+      ScaffoldMessenger.of(currentContext).showSnackBar(
+        SnackBar(
+          content: Text('Please enter a valid username.'),
+          backgroundColor: Colors.red,
+        ),
+      );
+    }
 
-  // if (response.statusCode == 200) {
-  //   Navigator.pushReplacement(context, MaterialPageRoute(builder: (_) => const HomeForm()));
-  // } else {
-  //   print('Failed to login');
-  // }
+    // if (response.statusCode == 200) {
+    //   Navigator.pushReplacement(context, MaterialPageRoute(builder: (_) => const HomeForm()));
+    // } else {
+    //   print('Failed to login');
+    // }
   }
 
   @override
@@ -85,11 +86,10 @@ class _LoginFormState extends State<LoginForm> {
                       fontSize: 25.0),
                 ),
                 GetTextFormField(
-                  controller: _conemail,
-                  icon: Icons.person,
-                  hintName: 'Email',
-                  inputtype: TextInputType.text
-                ),
+                    controller: _conemail,
+                    icon: Icons.person,
+                    hintName: 'Email',
+                    inputtype: TextInputType.text),
                 const SizedBox(height: 5.0),
                 GetTextFormField(
                   controller: _conpassword,
@@ -97,25 +97,23 @@ class _LoginFormState extends State<LoginForm> {
                   hintName: 'mot de passe',
                   isObscureText: true,
                 ),
-                
                 Container(
                   child: const Center(child: Text('mot de pass oublié')),
                 ),
-                
                 Container(
-                    margin: const EdgeInsets.all(30.0),
-                    width: double.infinity,
-                    decoration: BoxDecoration(
+                  margin: const EdgeInsets.all(30.0),
+                  width: double.infinity,
+                  decoration: BoxDecoration(
                       color: const Color.fromARGB(255, 76, 77, 166),
                       borderRadius: BorderRadius.circular(30.0)),
-                    child: TextButton(
-                      onPressed: login, 
+                  child: TextButton(
+                      onPressed: login,
                       child: const Text(
                         'connexion',
-                        style: TextStyle(color: Color.fromARGB(255, 197, 198, 243)),
+                        style: TextStyle(
+                            color: Color.fromARGB(255, 197, 198, 243)),
                       )),
-                  ),
-
+                ),
                 Center(
                   child: Container(
                       child: TextButton(
@@ -127,6 +125,21 @@ class _LoginFormState extends State<LoginForm> {
                           },
                           child: const Text(
                             'Inscrivez-vous',
+                            style: TextStyle(
+                                color: Color.fromARGB(255, 197, 198, 243)),
+                          ))),
+                ),
+                Center(
+                  child: Container(
+                      child: TextButton(
+                          onPressed: () {
+                            Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                    builder: (_) =>  MyHomePage()));
+                          },
+                          child: const Text(
+                            'calendar',
                             style: TextStyle(
                                 color: Color.fromARGB(255, 197, 198, 243)),
                           ))),
