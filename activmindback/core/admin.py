a70832f02005d60e708d6d428745de95c498bc99
felix <felixberta@outlook.com>
2024-03-18 02:06:46 -0400

    Update search fields in TokenAdmin

--- activmindback/core/admin.py
+++ activmindback/core/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from rest_framework.authtoken.models import Token
 
 class TokenAdmin(admin.ModelAdmin):
-    search_fields = ('email',)  # Ajoutez les champs que vous souhaitez utiliser pour la recherche
+    search_fields = ('user__email',)  # Ajoutez les champs que vous souhaitez utiliser pour la recherche
 
 # Enregistrez le modèle Token avec TokenAdmin
 admin.site.register(Token, TokenAdmin)
