fe1928190e656370d581429d5b3db85e279234d3
felix <felixberta@outlook.com>
2024-03-18 01:55:53 -0400

    Add branch "felix_sprint-2" to CI workflow and update admin.py files

--- activmindback/core/admin.py
+++ activmindback/core/admin.py
@@ -1,4 +1,9 @@
 from django.contrib import admin
+from rest_framework.authtoken.models import Token
 
-# Register your models here.
+class TokenAdmin(admin.ModelAdmin):
+    search_fields = ('email')  # Ajoutez les champs que vous souhaitez utiliser pour la recherche
+
+# Enregistrez le modèle Token avec TokenAdmin
+admin.site.register(Token, TokenAdmin)
 
