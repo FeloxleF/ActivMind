6cd1f133d6416de435f5d3dae7cc0d2ab4aad791
felix <felixberta@outlook.com>
2024-03-18 01:56:58 -0400

    Remove unnecessary model registrations

--- activmindback/users/admin.py
+++ activmindback/users/admin.py
@@ -9,8 +9,3 @@ from core.models import CustomUser, UserInfo, Task, Sport, Activity, Medicament
 admin.site.register(CustomUser)
 admin.site.register(UserInfo)
 
-# Register Task, Sport, Activity, and Medicament models
-admin.site.register(Task)
-admin.site.register(Sport)
-admin.site.register(Activity)
-admin.site.register(Medicament)
