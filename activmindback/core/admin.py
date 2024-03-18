2435e842fd5e8df943b89d286ff60d5aa133c99f
felix <felixberta@outlook.com>
2024-03-18 03:13:29 -0400

    Update TokenAdmin in admin.py and django version in requirements.txt

--- activmindback/core/admin.py
+++ activmindback/core/admin.py
@@ -4,6 +4,4 @@ from rest_framework.authtoken.models import Token
 class TokenAdmin(admin.ModelAdmin):
     search_fields = ('token',)  # Ajoutez les champs que vous souhaitez utiliser pour la recherche
 
-# Enregistrez le modèle Token avec TokenAdmin
-admin.site.register(Token, TokenAdmin)
 
