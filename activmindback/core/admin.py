040ae252ad9aa250ecdf4e6be0c03247833de3a3
felix <felixberta@outlook.com>
2024-03-18 03:36:19 -0400

    Update search_fields in TokenAdmin

--- activmindback/core/admin.py
+++ activmindback/core/admin.py
@@ -2,6 +2,6 @@ from django.contrib import admin
 from rest_framework.authtoken.models import Token
 
 class TokenAdmin(admin.ModelAdmin):
-    search_fields = ('token',)  # Ajoutez les champs que vous souhaitez utiliser pour la recherche
+    search_fields = ['token']  # Ajoutez les champs que vous souhaitez utiliser pour la recherche
 
 
